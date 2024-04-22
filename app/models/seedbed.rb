9de2f524dfcfee882dddf6ad3acb1efc4807654a
Anderson <andersond912@gmail.com>
2024-04-22 02:21:29 -0500

    Añadir la gema 'annotate' al Gemfile y Gemfile.lock

--- app/models/seedbed.rb
+++ app/models/seedbed.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: seedbeds
+#
+#  id             :bigint           not null, primary key
+#  name           :string
+#  acronym        :string
+#  research_group :string
+#  research_area  :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class Seedbed < ApplicationRecord
+  has_many :trainings
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: trainings
+#
+#  id                    :bigint           not null, primary key
+#  name                  :string
+#  description           :text
+#  start_date_enrollment :date
+#  end_date_enrollment   :date
+#  start_training_date   :date
+#  end_training_date     :date
+#  seedbed_id            :bigint           not null
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
 class Training < ApplicationRecord
   belongs_to :seedbed
 end
